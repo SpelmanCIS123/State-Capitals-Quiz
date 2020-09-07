53c97dd4761b4a553604f8efff9ba2d352b9cbfb
andreaej <andreaej@users.noreply.github.com>
2020-09-07 15:37:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,31 @@
 
 #Your algorithm should go here OR you should use comments throughout
 
-#Your code goes here
+import random
+
+# creating capitals dictionary
+state_capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau',
+                  'Arizona': 'Phoenix', 'Arkansas': 'Little Rock',
+                  'California': 'Sacramento', 'Colorado': 'Denver',
+                  'Connecticut': 'Hartford', 'Delaware': 'Dover',
+                  'Florida': 'Tallahassee', 'Georgia': 'Atlanta',
+                  'Hawaii': 'Honolulu', 'Idaho': 'Boise',
+                  'Illinois': 'Springfield', 'Indiana': 'Indianapolis',
+                  'Iowa': 'Des Moines', 'Kansas': 'Topeka',
+                  'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge',
+                  'Maine': 'Augusta', 'Maryland': 'Annapolis',
+                  'Massachusetts': 'Boston', 'Michigan': 'Lansing',
+                  'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson',
+                  'Missouri': 'Jefferson City', 'Montana': 'Helena',
+                  'Nebraska': 'Lincoln', 'Nevada': 'Carson City',
+                  'New Hampshire': 'Concord', 'New Jersey': 'Trenton',
+                  'New Mexico': 'Santa Fe', 'New York': 'Albany',
+                  'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck',
+                  'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+                  'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg',
+                  'Rhode Island': 'Providence', 'South Carolina': 'Columbia',
+                  'South Dakota': 'Pierre', 'Tennessee': 'Nashville',
+                  'Texas': 'Austin', 'Utah': 'Salt Lake City',
+                  'Vermont': 'Montpelier', 'Virginia': 'Richmond',
+                  'Washington': 'Olympia', 'West Virginia': 'Charleston',
+                  'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
